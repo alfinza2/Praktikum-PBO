99f5d80260af654538d121de63881037b979b9e8
alfin <75205848+alfinza2@users.noreply.github.com>
2021-10-20 00:56:12 +0800

    Add files via upload

---
+++ Pertemuan7/Pertemuan7/src/percobaan1/Karyawan.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan1;
+
+/**
+ *
+ * @author Alfinza SP
+ */
+public class Karyawan {
+    private String nama;
+    private String nip;
+    private String golongan;
+    private double gaji;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public void getGolongan(String golongan) {
+        this.golongan = golongan;
+        
+        switch(golongan.char)
+    }
+
+    public void setGolongan(String golongan) {
+        this.golongan = golongan;
+    }
+
+    public double getGaji() {
+        return gaji;
+    }
+
+    public void setGaji(double gaji) {
+        this.gaji = gaji;
+    }
+    
+    
+    
+}
